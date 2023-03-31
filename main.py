5dc7c13f7886c7d4d841487cd53949e3b87c7496
RxiPland <liborvanura@gmail.com>
2023-03-31 22:14:18 +0200

    updated example

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
+import random
 
 import autoskola_etesty as etesty
 
 
 for i in range(5):
-    print(etesty.get_question())
+    print(etesty.get_question(random.randint(1, 7), random.randint(1, 7)))
     print()
 
-    time.sleep(0.5)
+    time.sleep(0.4)
