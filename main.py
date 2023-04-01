7501516ecdfe15d80f49f48872c14eb5dcef861d
RxiPland <liborvanura@gmail.com>
2023-04-01 10:40:01 +0200

    updated example script

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import time
 import random
 
 import autoskola_etesty as etesty
 
 
-for i in range(5):
-    print(etesty.get_question(random.randint(1, 7), random.randint(1, 7)))
-    print()
+# Pravidla provozu na pozemních komunikacích (question_topic_id = 1)
+print(etesty.get_question(question_topic_id=1, previous_topic_id=random.randint(1, 7)))
+print()
 
-    time.sleep(0.4)
+# Dopravní značky (question_topic_id = 2)
+print(etesty.get_question(question_topic_id=2, previous_topic_id=random.randint(1, 7)))
+print()
+
+# Zásady bezpečné jízdy (question_topic_id = 3)
+print(etesty.get_question(question_topic_id=3, previous_topic_id=random.randint(1, 7)))
+print()
+
+# Dopravní situace (question_topic_id = 4)
+print(etesty.get_question(question_topic_id=4, previous_topic_id=random.randint(1, 7)))
+print()
+
+# Předpisy o podmínkách provozu vozidel (question_topic_id = 5)
+print(etesty.get_question(question_topic_id=5, previous_topic_id=random.randint(1, 7)))
+print()
+
+# Předpisy související s provozem (question_topic_id = 6)
+print(etesty.get_question(question_topic_id=6, previous_topic_id=random.randint(1, 7)))
+print()
+
+# Zdravotnická příprava (question_topic_id = 7)
+print(etesty.get_question(question_topic_id=7, previous_topic_id=random.randint(1, 7)))
+print()
