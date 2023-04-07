a8dee9f85d71ec74a798555b1187bafea822e7ca
RxiPland <liborvanura@gmail.com>
2023-04-07 11:05:29 +0200

    added example usage of new function

--- example.py
+++ example.py
@@ -31,6 +31,11 @@ print()
 print(etesty.get_random_question(question_topic_id=7))
 print()
 
+# Question based on URL
+url = "https://www.autoskola-testy.cz/prohlizeni_otazek.php?otazka=336-tato_dopravni_znacka_upozornuje_na_misto_kde"
+print(etesty.get_question(url))
+print()
+
 # All questions urls
 print(etesty.get_questions_urls(questions_topic_id=7))
 print()
